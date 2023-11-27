f9ab15eba06e2305a7e759ab3ac1bf53bed00117
Najma Saeed <najmasy20@gmail.com>
2023-11-27 10:22:36 +0000

    apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center ">
-      <p>You&apos;'re looking at my root route.</p>
+      <p>You&apos;re looking at my root route.</p>
     </main>
   );
 }
